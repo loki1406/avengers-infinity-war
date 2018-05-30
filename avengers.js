3d944b125811e2cf9db8b3e1ad8355f315ac0dd9
Lokesh Kuna <lokeshkuna1406@gmail.com>
2018-05-31 03:33:15 +0530

    end of first day.
    Created home page with search bar and predictions.

---
+++ avengers.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+  $("#searchbox").keyup(function(){
+    $('#predictions').html('');
+    var avengers = ['Iron Man',
+                    'Star-Lord',
+                    'Doctor Strange',
+                    'Captain America',
+                    'Black Widow',
+                    'Thor',
+                    'Spider-man',
+                    'Thanos',
+                    'Scarlet witch',
+                    'Winter Soldier',
+                    'Loki',
+                    'Hulk',
+                    'Black Panther',
+                    'Gamora',
+                    'Vision',
+                    'War Machine',
+                    'Drax the Destroyer',
+                    'Nebula',
+                    'Mantis',
+                    'Falcon',
+                    'Groot',
+                    'Rocket Raccoon',
+                    'Heimdall',
+                    'Okoye',
+                    'Wong'
+                  ];
+    var textinput = $('#searchbox').val();
+    var expression = new RegExp(textinput, "i");
+
+    $.each(avengers, function(key,value){
+      if(value.search(expression) !== -1 && textinput !== ''){
+        $('#predictions').append('<li class = "dropdown"><a href="">'+value+'</a></li>');
+      }
+    });
+
+
+  });
+});
